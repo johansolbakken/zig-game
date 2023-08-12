37696c793e1f987d2b458696f368e3b4d36cdff8
Johan Solbakken <johan.hvaler@gmail.com>
2023-08-12 03:00:51 +0200

    Add plumming for future multi graphics api support

--- src/renderer/rendercommand.zig
+++ src/renderer/rendercommand.zig
@@ -1,13 +1,29 @@
 const opengl = @import("../platform/opengl/opengl.zig");
 
+const Api = enum {
+    OpenGL,
+    Vulkan,
+};
+
+const api = Api.OpenGL;
+
 pub fn init() !void {
-    try opengl.init();
+    switch (api) {
+        Api.OpenGL => try opengl.init(),
+        else => error.UnsupportedApi,
+    }
 }
 
 pub fn setClearColor(r: f32, g: f32, b: f32, a: f32) void {
-    opengl.setClearColor(r, g, b, a);
+    switch (api) {
+        Api.OpenGL => opengl.setClearColor(r, g, b, a),
+        else => error.UnsupportedApi,
+    }
 }
 
 pub fn clear() void {
-    opengl.clear();
+    switch (api) {
+        Api.OpenGL => opengl.clear(),
+        else => error.UnsupportedApi,
+    }
 }
