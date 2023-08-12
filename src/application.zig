9541a0552d4b2c72458aed1f51b3988873924b76
Johan Solbakken <johan.hvaler@gmail.com>
2023-08-12 02:43:45 +0200

    Add application file

---
+++ src/application.zig
@@ -0,0 +1,29 @@
+const Window = @import("window.zig").Window;
+const Renderer = @import("renderer.zig").Renderer;
+
+pub const Application = struct {
+    const Self = @This();
+
+    window: Window,
+    renderer: Renderer,
+
+    pub fn init() !Self {
+        const window = try Window.init("My game", 800, 600);
+        const renderer = try Renderer.init();
+        return .{
+            .window = window,
+            .renderer = renderer,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.renderer.deinit();
+        self.window.deinit();
+    }
+
+    pub fn run(self: *Self) !void {
+        while (!self.window.shouldClose()) {
+            self.window.update();
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,7 @@
-const std = @import("std");
-const Window = @import("window.zig").Window;
-const Renderer = @import("renderer.zig").Renderer;
+const Application = @import("application.zig").Application;
 
 pub fn main() !void {
-    var window = try Window.init("Hello World!", 800, 600);
-    defer window.deinit();
-
-    var renderer = try Renderer.init();
-    defer renderer.deinit();
-
-    while (!window.shouldClose()) {
-        window.update();
-    }
+    var app = try Application.init();
+    defer app.deinit();
+    try app.run();
 }
